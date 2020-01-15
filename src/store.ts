3b317853168d501d598e4bba06bfe3fbbacea006
Deivid Byrdarov <deivid.byrdarov@gmail.com>
2020-01-15 22:13:26 +0200

    redux-fix

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,14 @@
 import thunk from "redux-thunk"
 import { persistStore } from "redux-persist"
-import { createStore, applyMiddleware, compose } from "redux"
+import { createStore, applyMiddleware } from "redux"
+import { composeWithDevTools } from "redux-devtools-extension"
 
 import rootReducer from "./redux/reducers"
 
-const devTools = process.env.NODE_ENV === "development" ? (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null
-
 const store = createStore(
   rootReducer,
-  compose(
+  composeWithDevTools(
     applyMiddleware(thunk),
-    devTools,
   ),
 )
 
