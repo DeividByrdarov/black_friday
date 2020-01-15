fafab94221041ab8551c73631bb988c8a06b8276
Deivid Byrdarov <deivid.byrdarov@gmail.com>
2020-01-15 22:07:20 +0200

    redux fix

--- src/store.ts
+++ src/store.ts
@@ -4,7 +4,7 @@ import { createStore, applyMiddleware, compose } from "redux"
 
 import rootReducer from "./redux/reducers"
 
-const devTools = process.env.NODE_ENV === "development" ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null
+const devTools = process.env.NODE_ENV === "development" ? (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null
 
 const store = createStore(
   rootReducer,
